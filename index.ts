43e840bcf8ae19b4cf16eb9bb72b166729840d33
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-07-10 18:28:07 +0200

    get direct intermediate token to add more legs

--- index.ts
+++ index.ts
@@ -18,44 +18,43 @@ const wallet = new NodeWallet(
 const USDC_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 const SOL_MINT = "So11111111111111111111111111111111111111112";
 const initial = 50_000_000;
-const minimal_output = initial * 1.0005
+const minimal_output = initial * 1.0015
 const profit_wanted = minimal_output / 1000000;
-//const indexedRoute = await getIndexedRoutes()
 
+const indexedRoute = await getIndexedRoutes();
+const SOL_POSITION = indexedRoute.mintKeys.indexOf("DUSTawucrTsGU8hcqRdHDCbuYhCPADMLM2VcCb8VnFnQ");
+const INDEXES_INTERMEDIATE = indexedRoute.indexedRouteMap[SOL_POSITION];
+let MINT_KEYS: string[] = []
+for (const intermediate in INDEXES_INTERMEDIATE) {
+  const value = INDEXES_INTERMEDIATE[intermediate];
+  const key: string = indexedRoute.mintKeys[value]
+  MINT_KEYS.push(key);
+}
 
-/*async function getIndexedRoutes(){
-  const options = {
-    method: "get",
-  };
+async function getIndexedRoutes(){
   return await (
     await fetch(
-      "https://quote-api.jup.ag/v1/indexed-route-map?onlyDirectRoutes=true", 
-      options
+      "https://quote-api.jup.ag/v1/indexed-route-map?onlyDirectRoutes=true", {
+        method: "GET",
+      }
     )
   ).json();
 }
 
-let i = 0;
-for (const route in indexedRoute.mintKeys[i]) {
-  console.log(route + ": " + i)
-  i++;
-}
-*/
 async function getQuotes(){
-  const options = {
-    method: "get",
-  };
   const usdcToSol = await (
     await fetch(
-      `https://quote-api.jup.ag/v1/quote?outputMint=${SOL_MINT}&inputMint=${USDC_MINT}&amount=${initial}&slippage=0.2&onlyDirectRoutes=true`,
-      options
+      `https://quote-api.jup.ag/v1/quote?outputMint=${SOL_MINT}&inputMint=${USDC_MINT}&amount=${initial}`, { //&onlyDirectRoutes=true
+        method: "GET",
+      }
     )
   ).json();
 
   const solToUsdc = await (
     await fetch(
-      `https://quote-api.jup.ag/v1/quote?outputMint=${USDC_MINT}&inputMint=${SOL_MINT}&amount=${usdcToSol.data[0].outAmount}&slippage=0.2&onlyDirectRoutes=true`,
-      options
+      `https://quote-api.jup.ag/v1/quote?outputMint=${USDC_MINT}&inputMint=${SOL_MINT}&amount=${usdcToSol.data[0].outAmount}`, {
+        method: "GET",
+      }
     )
   ).json();
 
@@ -67,7 +66,7 @@ async function getQuotes(){
 async function getTransaction(route){
   return await (
     await fetch("https://quote-api.jup.ag/v1/swap", {
-      method: "post",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
@@ -108,8 +107,7 @@ while (true) {
       [usdcToSol, solToUsdc].map(async (route) => {
         const tx = await getTransaction(route);
         console.log(tx);
-        console.log("1:"+ tx.setupTransaction, "2:"+ tx.swapTransaction, "3:"+ tx.cleanupTransaction)
-        await Promise.all(
+        await Promise.all(// i think setupTx creates necessary ATA's and cleanupTx closes it, if the necessary ATA's are already created you will get those attributes as undefined
           [tx.setupTransaction, tx.swapTransaction, tx.cleanupTransaction]
             .filter(Boolean)
             .map(async (serializedTransaction) => {

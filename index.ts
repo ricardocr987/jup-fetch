f5404a844be03fee960b76741308cd14f479bdd6
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-07-10 16:25:04 +0200

    improved performance

--- index.ts
+++ index.ts
@@ -17,42 +17,63 @@ const wallet = new NodeWallet(
 
 const USDC_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 const SOL_MINT = "So11111111111111111111111111111111111111112";
-const pubkey = "7SZM2pvDUfonYmdPqxwaw8bp93JvUXWiWWyRSHfga2Q7"
-const initial = 10_000_000;
+const initial = 50_000_000;
+const minimal_output = initial * 1.0005
+const profit_wanted = minimal_output / 1000000;
+//const indexedRoute = await getIndexedRoutes()
 
+
+/*async function getIndexedRoutes(){
+  const options = {
+    method: "get",
+  };
+  return await (
+    await fetch(
+      "https://quote-api.jup.ag/v1/indexed-route-map?onlyDirectRoutes=true", 
+      options
+    )
+  ).json();
+}
+
+let i = 0;
+for (const route in indexedRoute.mintKeys[i]) {
+  console.log(route + ": " + i)
+  i++;
+}
+*/
 async function getQuotes(){
   const options = {
     method: "get",
   };
   const usdcToSol = await (
     await fetch(
-      `https://quote-api.jup.ag/v1/quote?outputMint=${SOL_MINT}&inputMint=${USDC_MINT}&amount=${initial}&slippage=0.2`,
+      `https://quote-api.jup.ag/v1/quote?outputMint=${SOL_MINT}&inputMint=${USDC_MINT}&amount=${initial}&slippage=0.2&onlyDirectRoutes=true`,
       options
     )
   ).json();
 
   const solToUsdc = await (
     await fetch(
-      `https://quote-api.jup.ag/v1/quote?outputMint=${USDC_MINT}&inputMint=${SOL_MINT}&amount=${usdcToSol.data[0].outAmount}&slippage=0.2`,
+      `https://quote-api.jup.ag/v1/quote?outputMint=${USDC_MINT}&inputMint=${SOL_MINT}&amount=${usdcToSol.data[0].outAmount}&slippage=0.2&onlyDirectRoutes=true`,
       options
     )
   ).json();
 
-  console.log("Output: " + solToUsdc.data[0].outAmount + " | Market: " + solToUsdc.data[0].marketInfos[0].label + " | Time taken: " + solToUsdc.timeTaken);
+  console.log("Output: " + solToUsdc.data[0].outAmount/1000000 + " | Wanted: " + profit_wanted + " | Market: " + solToUsdc.data[0].marketInfos[0].label + " | Time taken: " + solToUsdc.timeTaken);
 
   return [ usdcToSol.data[0], solToUsdc.data[0] ];
 }
 
-async function getTransaction(route, pubkey){
+async function getTransaction(route){
   return await (
     await fetch("https://quote-api.jup.ag/v1/swap", {
-      method: "POST",
+      method: "post",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         route: route,
-        userPublicKey: pubkey,
+        userPublicKey: "7SZM2pvDUfonYmdPqxwaw8bp93JvUXWiWWyRSHfga2Q7",
         wrapUnwrapSOL: false,
       }),
     })
@@ -61,17 +82,10 @@ async function getTransaction(route, pubkey){
 
 const getConfirmTransaction = async (txid) => {
   const res = await promiseRetry(
-    async (retry) => {
+    async () => {
       let txResult = await connection.getTransaction(txid, {
         commitment: "confirmed",
       });
-
-      if (!txResult) {
-        const error = new Error("Transaction was not confirmed");
-
-        retry(error);
-        return;
-      }
       return txResult;
     },
     {
@@ -88,11 +102,13 @@ const getConfirmTransaction = async (txid) => {
 
 while (true) {
   const [ usdcToSol, solToUsdc ] = await getQuotes()
-  if (solToUsdc.outAmount > initial + 15000) {
+  if (solToUsdc.outAmount > minimal_output) {
     console.log("Opportunity found");
     await Promise.all(
       [usdcToSol, solToUsdc].map(async (route) => {
-        const tx = await getTransaction(route, pubkey);
+        const tx = await getTransaction(route);
+        console.log(tx);
+        console.log("1:"+ tx.setupTransaction, "2:"+ tx.swapTransaction, "3:"+ tx.cleanupTransaction)
         await Promise.all(
           [tx.setupTransaction, tx.swapTransaction, tx.cleanupTransaction]
             .filter(Boolean)
